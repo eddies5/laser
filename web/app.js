165ea7db9242efbf12d0b63c184e886e269e7ee0
Eddie <saenzeddie5@gmail.com>
2013-12-24 12:42:22 -0600

    added socket.io

--- web/app.js
+++ web/app.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
 
 app.use(express.logger());
 
 app.get('/', function(req, res){
-  res.sendfile('./index.html');
+  res.sendfile(__dirname + '/index.html');
 });
 
-app.listen(3000);
+server.listen(3000);
 console.log('Listening on port 3000');
