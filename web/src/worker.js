3a0bf3e97c8f9ebe8bd991f1e6defb2a80c24a65
Eddie <saenzeddie5@gmail.com>
2014-01-24 22:00:25 -0600

    movement working

--- web/src/worker.js
+++ web/src/worker.js
@@ -1,4 +1,9 @@
 
+var SerialPort = require('serialport').SerialPort;
+var serialPort = new SerialPort("/dev/tty.usbmodem641", {
+	baudrate: 9600
+});
+
 /**
  * Worker constructor.
  *
@@ -26,18 +31,50 @@ function Worker (jobs) {
 
 			socket.on('left', function (data) {
 				console.log('left');
+				serialPort.write("1", function (err, results) {
+					console.log('err: ' + err);
+					console.log('results: ' + results);
+				});
+				serialPort.write("2", function (err, results) {
+					console.log('err: ' + err);
+					console.log('results: ' + results);
+				});
 			});
 
 			socket.on('right', function (data) {
 				console.log('right');
+				serialPort.write("1", function (err, results) {
+					console.log('err: ' + err);
+					console.log('results: ' + results);
+				});
+				serialPort.write("3", function (err, results) {
+					console.log('err: ' + err);
+					console.log('results: ' + results);
+				});
 			});
 
 			socket.on('up', function (data) {
 				console.log('up');
+				serialPort.write("0", function (err, results) {
+					console.log('err: ' + err);
+					console.log('results: ' + results);
+				});
+				serialPort.write("4", function (err, results) {
+					console.log('err: ' + err);
+					console.log('results: ' + results);
+				});
 			});
 
 			socket.on('down', function (data) {
 				console.log('down');
+				serialPort.write("0", function (err, results) {
+					console.log('err: ' + err);
+					console.log('results: ' + results);
+				});
+				serialPort.write("5", function (err, results) {
+					console.log('err: ' + err);
+					console.log('results: ' + results);
+				});
 			});
 
 			socket.on('disconnect', function () {
