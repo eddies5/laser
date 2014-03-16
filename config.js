d60dd434df4876f21509a5fd8da7338f471d5c6f
Eddie <saenzeddie5@gmail.com>
2014-03-15 19:03:13 -0500

    changed default arduino port

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ config.webSocket.port = 3050;
 config.streamServer.port = 3075;
 config.streamServer.host = process.env.STREAM_HOST || '127.0.0.1';
 
-config.arduinoSerialPort = process.env.ARDUINO_PORT || '/dev/tty.usbmodem641';
+config.arduinoSerialPort = process.env.ARDUINO_PORT || '/dev/ttyACM0';
 config.kuePort = 3000;
 
 module.exports = config;
